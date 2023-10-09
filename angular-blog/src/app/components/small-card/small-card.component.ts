cc3852320cf13ba06fbd11b8e79bf22976a1baf2
AdrianoSilva <adrianomagalhaesboy@gmail.com>
2023-10-09 13:22:30 -0300

    content created

--- angular-blog/src/app/components/small-card/small-card.component.ts
+++ angular-blog/src/app/components/small-card/small-card.component.ts
@@ -9,8 +9,13 @@ export class SmallCardComponent implements OnInit {
 
   @Input()
   photoCover:string= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ28WA2ZQREgEZ1jva2HNK6hzzNLXtnkxGhG2eCg1bAuw&s"
+
   @Input()
   cardTitle:string= ""
+
+  @Input()
+  id:string="0"
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://www.hqzona.com.br/wp-content/uploads/2020/05/star-wars-feature-image.jpg"
+  },
+  {
+    "id":"2",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "https://s2-techtudo.glbimg.com/ektK9m6xzWpmrk2QStaFZ5_JDes=/0x0:1239x977/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/W/5/81M9kaRcAAjPJObwNXhg/ahsoka-rt-techtudo-02.jpg"
+  }
+]
+
